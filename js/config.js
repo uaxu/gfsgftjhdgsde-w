7b8b6d7654ae1019a929fe1033a89f15ee44c7ad
uaxu <117697424+uaxu@users.noreply.github.com>
2026-07-15 19:35:47 +0100

    Update music paths and user buttons in config.js

--- js/config.js
+++ js/config.js
@@ -17,13 +17,181 @@ const PROFILES = {
         name: 'pulse',
         group: 'Consortia',
         image: '/assets/pulse.png',
-        music: '/assets/pulse_track.mp3',
+        music: '/assets/pulse.mp3',
         titleAnimation: ['p','pu','pul','puls','pulse','puls','pul','pu'],
         buttons: [
-            { icon: 'discord', label: '@pulse_discord' },
-            { icon: 'telegram', label: '@pulse_tg' },
-            { icon: 'gmail', label: 'pulse@onionmail[.]org' }
+            { icon: 'discord', label: '@1cyb' },
+            { icon: 'litecoin', label: 'LfoLWkSeSf4NmHgdmXsv1mxFYuckyVhf5a' },
+            { icon: 'roblox', label: 'https://www.roblox.com/users/2793063959/profile' }
+        ]
+    },
+    tsar: {
+        name: 'tsar',
+        group: 'Primera',
+        image: '/assets/tsar.png',
+        music: '/assets/tsar.mp3',
+        titleAnimation: ['t','ts','tsa','tsar','tsa','ts'],
+        buttons: [
+            { icon: 'roblox', label: 'https://roblox.com/users/7189303677/profile' },
+            { icon: 'instagram', label: 'https://instagram.com/upptsar/' }
+        ]
+    },
+    cia: {
+        name: 'cia',
+        group: 'Primera',
+        image: '/assets/cia.png',
+        music: '/assets/cia.mp3',
+        titleAnimation: ['c','ci','cia','ci'],
+        buttons: [
+            { icon: 'discord', label: '@trackmoney' }
+        ]
+    },
+    zarro: {
+        name: 'zarro',
+        group: 'Primera',
+        image: '/assets/zarro.png',
+        music: '/assets/zarro.mp3',
+        titleAnimation: ['z','za','zar','zarr','zarro','zarr','zar','za'],
+        buttons: []
+    },
+    neumann: {
+        name: 'neumann',
+        group: 'Primera',
+        image: '/assets/neumann.jpg',
+        music: '/assets/jamorant.mp3',
+        titleAnimation: ['n','ne','neu','neum','neuma','neuman','neumann','neuman','neuma','neum','neu','ne'],
+        buttons: [
+            { icon: 'roblox', label: 'https://roblox.com/users/1555469161/profile/' },
+            { icon: 'instagram', label: 'https://instagram.com/david_santos0003/' }
+        ]
+    },
+    persistive: {
+        name: 'persistive',
+        group: 'Primera',
+        image: '/assets/9npm.jpg',
+        music: '/assets/9npm.mp3',
+        titleAnimation: ['p','pe','per','pers','persi','persis','persist','persisti','persistiv','persisitive','persistiv','persisti','persist','persis','persi','pers','per','pe'],
+        buttons: [
+            { icon: 'discord', label: '@persistive' },
+            { icon: 'instagram', label: 'https://instagram.com/b5ive.otf/' },
+            { icon: 'snapchat', label: 'https://www.snapchat.com/@brunex.cc' }
+        ]
+    },
+    rootz: {
+        name: 'rootz',
+        group: 'Primera',
+        image: '/assets/rootz.gif',
+        music: '/assets/selling-dreams.mp3',
+        titleAnimation: ['r','ro','roo','root','rootz','root','roo','ro'],
+        buttons: [
+            { icon: 'roblox', label: 'https://roblox.com/users/66834927/profile' },
+            { icon: 'onlyfans', label: 'https://whbf.cc/discord' }
+        ]
+    },
+    zero: {
+        name: 'zero',
+        group: 'Sancta',
+        image: '/assets/zero.png',
+        music: '/assets/zero.mp3',
+        titleAnimation: ['z','ze','zer','zero','zer','ze'],
+        buttons: [
+            { icon: 'roblox', label: 'https://roblox.com/users/8524145/profile' },
+            { icon: 'github', label: 'https://github.com/luciferinprod' }
+        ]
+    },
+    cae: {
+        name: 'cae',
+        group: 'Sancta',
+        image: '/assets/catcae.webp',
+        music: '/assets/loudandclear.mp3',
+        titleAnimation: ['c','ca','cae','ca'],
+        buttons: [
+            { icon: 'roblox', label: 'https://roblox.com/users/682719517/profile' },
+            { icon: 'spacehey', label: 'https://spacehey.com/seraphsghoul' }
+        ]
+    },
+    ivan: {
+        name: 'ivan',
+        group: 'Sancta',
+        image: '/assets/ivan.png',
+        music: '/assets/trustmeproud.mp3',
+        titleAnimation: ['i','iv','iva','ivan','iva','iv'],
+        buttons: [
+            { icon: 'discord', label: '@mjds' },
+            { icon: 'solana', label: '92bfwZU97vDp8tu2Aq2UdteeUvbR8WuMkZ12x67wNLku' },
+            { icon: 'ethereum', label: '0x8a7943ef5945f7BA84586c456390F6cCE9d8b139' },
+            { icon: 'bitcoin', label: 'bc1qhkgzanasmxdu22jp0c48t693sgnsk7xpwf0auc' },
+            { icon: 'polygon', label: '0x9495843027c180C68c9F0bf2B42112806f86D94C' }
+        ]
+    },
+    lemes: {
+        name: 'lemes',
+        group: 'Sancta',
+        image: '/assets/lemes.png',
+        music: '/assets/lemes.mp3',
+        titleAnimation: ['l','le','lem','leme','lemes','leme','lem','le'],
+        buttons: [
+            { icon: 'discord', label: '@lemes.bfe' },
+            { icon: 'roblox', label: 'https://roblox.com/users/3229992738/profile' },
+            { icon: 'spotify', label: 'https://open.spotify.com/user/31gfslrtsaajnzxkycm354g5zjnm' }
+        ]
+    },
+    nyx: {
+        name: 'nyx',
+        group: 'Sancta',
+        image: '/assets/nyx.png',
+        music: '/assets/nyx.mp3',
+        titleAnimation: ['n','ny','nyx','ny'],
+        buttons: [
+            { icon: 'discord', label: '@nychiix' }
+        ]
+    },
+    gabe: {
+        name: 'gabe',
+        group: 'Sancta',
+        image: '/assets/gabe.png',
+        music: '/assets/gabe.mp3',
+        titleAnimation: ['g','ga','gab','gabe','gab','ga'],
+        buttons: [
+            { icon: 'roblox', label: 'https://roblox.com/users/1356120340/profile' },
+            { icon: 'spotify', label: 'https://open.spotify.com/user/313qxig55qlojpmfyfq5f7ff7ws4' }
+        ]
+    },
+    xer: {
+        name: 'xer',
+        group: 'Sancta',
+        image: '/assets/xer.jpg',
+        music: '/assets/xer.mp3',
+        titleAnimation: ['x','xe','xer','xe'],
+        buttons: [
+            { icon: 'roblox', label: 'https://roblox.com/users/445126985/profile' },
+            { icon: 'github', label: 'https://github.com/xer-7' }
+        ]
+    },
+    zenya: {
+        name: 'zenya',
+        group: 'Sancta',
+        image: '/assets/zenya.png',
+        music: '/assets/boundaryreceptor.mp3',
+        titleAnimation: ['z','ze','zen','zeny','zenya','zeny','zen','ze'],
+        buttons: [
+            { icon: 'roblox', label: 'https://roblox.com/users/1642616782/profile' },
+            { icon: 'namemc', label: 'https://namemc.com/profile/unmortful.1' },
+            { icon: 'steam', label: 'https://steamcommunity.com/id/unmortful' },
+            { icon: 'discord', label: 'https://discord.com/users/308653982622941184' },
+            { icon: 'pinterest', label: 'https://pinterest.com/unmortful/' }
+        ]
+    },
+    '404': {
+        name: '404',
+        group: 'Fidelis',
+        image: '/assets/404.png',
+        music: '/assets/alex.mp3',
+        titleAnimation: ['4','40','404','40'],
+        buttons: [
+            { icon: 'tiktok', label: 'https://tiktok.com/@12950.ngg' },
+            { icon: 'discord', label: 'https://discord.com/users/1066503806600368228' },
+            { icon: 'spotify', label: 'https://open.spotify.com/user/31zkf7xjvnrebepx3sdj6lcm3aoi' }
         ]
     }
-    // Add ALL your users here - 404 will automatically work for them!
 };
